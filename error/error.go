b7a25ffe2757e73793aaaf4a5bcb1dcb97c959b6
Brian Reece <bdreece@mtu.edu>
2022-04-28 01:24:11 -0400

    Added error module

---
+++ error/error.go
@@ -0,0 +1,16 @@
+package error
+
+import "fmt"
+
+type IndexOutOfBoundsError struct {
+	index  int
+	bounds int
+}
+
+func NewIndexOutOfBoundsError(index, bounds int) IndexOutOfBoundsError {
+	return IndexOutOfBoundsError{index, bounds}
+}
+
+func (e *IndexOutOfBoundsError) Error() string {
+	return fmt.Sprintf("index %d > bounds %d", e.index, e.bounds)
+}
---
+++ error/go.mod
@@ -0,0 +1,3 @@
+module github.com/bdreece/gollections/error
+
+go 1.18
