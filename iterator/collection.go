989806e461fd2f9c288bd82243a7c08ecd7d81e3
Brian Reece <bdreece@mtu.edu>
2022-05-05 18:35:28 -0400

    Updated iterator.Collection definition

--- iterator/collection.go
+++ iterator/collection.go
@@ -7,23 +7,7 @@ package iterator
 // all iterable collections.
 type Collection[T any] interface {
 	Iterator() Iterator[T]
-	Collect(...T)
-}
-
-// Collect returns a collection filled with the
-// items remaining in the iterator. Returns coll,
-// error on collection error.
-func Collect[T any, C Collection[T]](iter Iterator[T]) (*C, error) {
-	var (
-		err error
-	)
-	coll := new(C)
-	for {
-		item, err := iter.Next()
-		if err != nil || item == nil {
-			break
-		}
-		(*coll).Collect(*item)
-	}
-	return coll, err
+	Collect(Iterator[T]) error
+	Append(...T)
+	Extend(Collection[T]) error
 }
