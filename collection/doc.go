ea51e857e93bbca286ed1e178f7f69d75664441f
Brian Reece <bdreece@mtu.edu>
2022-05-05 21:16:01 -0400

    Added collection submodule

---
+++ collection/doc.go
@@ -0,0 +1,8 @@
+// MIT License
+// Copyright (c) 2022 Brian Reece
+
+/*
+collection provides interfaces for common functionality
+across different concrete collection types.
+*/
+package collection
---
+++ collection/go.mod
@@ -0,0 +1,5 @@
+module github.com/bdreece/gollections/collection
+
+go 1.18
+
+require github.com/bdreece/gollections/iterator v0.0.0-20220506003059-c0f192a6651e
