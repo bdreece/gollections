3cbb04bfd482a284b1cd78ac6c18abcdcf008d58
Brian Reece <bdreece@mtu.edu>
2022-05-09 21:37:32 -0400

    Updated list implementation to match interfaces

--- list/list.go
+++ list/list.go
@@ -14,46 +14,20 @@ type node[T any] struct {
 	prev  *node[T]
 }
 
-func newNode[T any](value T, next, prev *node[T]) *node[T] {
+func newNode[T any](value T, prev, next *node[T]) *node[T] {
 	return &node[T]{value, next, prev}
 }
 
 // List is the doubly-linked list data structure.
 type List[T any] struct {
 	head   *node[T]
+	tail   *node[T]
 	length int
 }
 
 // New constructs a new List
 func New[T any]() *List[T] {
-	return &List[T]{head: nil, length: 0}
-}
-
-// Front returns a pointer to the front
-// of the List. Returns nil, errors.Empty
-// if the List is empty.
-func (l *List[T]) Front() (*T, error) {
-	if l.head == nil {
-		return nil, errors.Empty{}
-	}
-
-	return &l.head.value, nil
-}
-
-// Back returns a pointer to the back
-// of the List. Returns nil, errors.Empty
-// if the List is empty.
-func (l *List[T]) Back() (*T, error) {
-	if l.head == nil {
-		return nil, errors.Empty{}
-	}
-
-	walk := l.head
-	for walk.next != nil {
-		walk = walk.next
-	}
-
-	return &walk.value, nil
+	return &List[T]{head: nil, tail: nil, length: 0}
 }
 
 // PushBack pushes an item onto the back
@@ -62,27 +36,56 @@ func (l *List[T]) PushBack(item T) {
 	l.length += 1
 	if l.head == nil {
 		l.head = newNode(item, nil, nil)
+		l.tail = l.head
 		return
 	}
 
-	walk := l.head
-	for walk.next != nil {
-		walk = walk.next
+	l.tail.next = newNode(item, l.tail, nil)
+	l.tail = l.tail.next
+}
+
+// PopBack removes and returns an item from
+// the back of the List. Returns nil, errors.Empty
+// if the List is empty.
+func (l *List[T]) PopBack() (*T, error) {
+	if l.tail == nil {
+		return nil, errors.Empty{}
+	}
+	value := new(T)
+	*value = l.tail.value
+
+	// Recede tail
+	l.tail = l.tail.prev
+
+	// Unlink removed tail
+	l.tail.next = nil
+
+	l.length -= 1
+	return value, nil
+}
+
+// PeekBack returns a pointer to the back
+// of the List. Returns nil, errors.Empty
+// if the List is empty.
+func (l *List[T]) PeekBack() (*T, error) {
+	if l.tail == nil {
+		return nil, errors.Empty{}
 	}
 
-	walk.next = newNode(item, nil, walk)
+	return &l.tail.value, nil
 }
 
 // PushFront pushes an item onto the front
 // of the double-linked list.
 func (l *List[T]) PushFront(item T) {
-	newHead := newNode(item, l.head, nil)
-	if l.head != nil {
-		newHead.next = l.head
-		l.head.prev = newHead
-	}
-	l.head = newHead
 	l.length += 1
+	if l.head != nil {
+		l.head = newNode(item, nil, nil)
+		l.tail = l.head
+		return
+	}
+	l.head.prev = newNode(item, nil, l.head)
+	l.head = l.head.prev
 }
 
 // PopFront removes and returns an item from
@@ -95,30 +98,54 @@ func (l *List[T]) PopFront() (*T, error) {
 
 	value := new(T)
 	*value = l.head.value
+
+	// Advance head
 	l.head = l.head.next
+	// Unlink deleted head
+	l.head.prev = nil
+
 	l.length -= 1
 	return value, nil
 }
 
-// PopBack removes and returns an item from
-// the back of the List. Returns nil, errors.Empty
+// PeekFront returns a pointer to the front
+// of the List. Returns nil, errors.Empty
 // if the List is empty.
-func (l *List[T]) PopBack() (*T, error) {
+func (l *List[T]) PeekFront() (*T, error) {
 	if l.head == nil {
 		return nil, errors.Empty{}
 	}
 
-	walk := l.head
-	for walk.next != nil {
-		walk = walk.next
-	}
+	return &l.head.value, nil
+}
 
-	if walk.prev != nil {
-		walk.prev.next = nil
-		walk.prev = nil
-	}
-	l.length -= 1
-	return &walk.value, nil
+// Enqueue appends an item to the end of
+// the list.
+func (l *List[T]) Enqueue(elem T) {
+	l.PushBack(elem)
+}
+
+// Dequeue removes and returns the first
+// element in the list.
+func (l *List[T]) Dequeue() (*T, error) {
+	return l.PopFront()
+}
+
+// Push prepends the list with an element.
+func (l *List[T]) Push(elem T) {
+	l.PushFront(elem)
+}
+
+// Pop removes and returns the first
+// element in the list.
+func (l *List[T]) Pop() (*T, error) {
+	return l.PopFront()
+}
+
+// Peek returns the first element in
+// the list without removing it.
+func (l *List[T]) Peek() (*T, error) {
+	return l.PeekFront()
 }
 
 // Get returns a pointer to an item in the
@@ -166,33 +193,72 @@ func (l *List[T]) Set(index int, item T) error {
 	return nil
 }
 
-// Collect collects items from an iterator into the list.
-func (l *List[T]) Collect(iter iterator.Iterator[T]) error {
+// Ins inserts an element into the list at the specified
+// index. Returns errors.IndexOutOfBounds if index < 0
+// or index > len(l).
+func (l *List[T]) Ins(index int, elem T) error {
+	if l.head == nil {
+		return errors.Empty{}
+	}
+
+	walk := l.head
+	for i := 0; i < index; i++ {
+		if walk.next == nil {
+			return errors.IndexOutOfBounds{Index: index, Bounds: i}
+		}
+		walk = walk.next
+	}
+
+	item := newNode[T](elem, walk.prev, walk)
+	item.prev.next = item
+	walk.prev = item
+	return nil
+}
+
+// Del removes and returns an element from the list at the
+// specified index. Returns errors.IndexOutOfBounds if
+// index < 0 or index > len(l).
+func (l *List[T]) Del(index int) (*T, error) {
+	if l.head == nil {
+		return nil, errors.Empty{}
+	}
+
+	walk := l.head
+	for i := 0; i < index; i++ {
+		if walk.next == nil {
+			return nil, errors.IndexOutOfBounds{Index: index, Bounds: i}
+		}
+		walk = walk.next
+	}
+	value := new(T)
+	*value = walk.value
+
+	// Unlink neighbors
+	walk.prev.next = nil
+	walk.next.prev = nil
+
+	return value, nil
+}
+
+// Collect appends a variable number of items to the list.
+func (l *List[T]) Collect(items ...T) {
+	for _, item := range items {
+		l.PushBack(item)
+	}
+}
+
+// IntoIterator returns an iterator over the items in the list.
+func (l *List[T]) IntoIterator() iterator.Iterator[T] {
+	return &Iterator[T]{l.head}
+}
+
+// FromIterator collects the items from the iterator into the
+// list.
+func (l *List[T]) FromIterator(iter iterator.Iterator[T]) error {
 	if err := iterator.ForEach(iter, func(item *T) {
 		l.PushBack(*item)
 	}); err != nil {
 		return err
 	}
 	return nil
-}
-
-// Append appends a variable number of items to the list.
-func (l *List[T]) Append(items ...T) {
-	for _, item := range items {
-		l.PushBack(item)
-	}
-}
-
-func (l *List[T]) Extend(coll iterator.Collection[T]) error {
-	if err := iterator.ForEach(coll.Iterator(), func(item *T) {
-		l.PushBack(*item)
-	}); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Iterator returns an iterator over the items in the list.
-func (l *List[T]) Iterator() iterator.Iterator[T] {
-	return &Iterator[T]{l.head}
 }
