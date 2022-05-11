f23ce0cd79c599b611b8525aeb10841be3b749ab
Brian Reece <bdreece@mtu.edu>
2022-05-10 21:04:06 -0400

    Updated ordered map testing suite

--- maps/ordered/iterator_test.go
+++ maps/ordered/iterator_test.go
@@ -23,21 +23,6 @@ func TestForEach(t *testing.T) {
 	}
 }
 
-func TestZip(t *testing.T) {
-	ours, theirs := setup()
-
-	if err := iterator.ForEach[iterator.ZipItem[string, int]](
-		iterator.NewZip(ours.Keys(), ours.Vals()),
-		func(item *iterator.ZipItem[string, int]) {
-			if *item.B != theirs[*item.A] {
-				t.Errorf(EXPECTED, "val", theirs[*item.A], *item.B)
-			}
-		},
-	); err != nil {
-		t.Errorf(ERROR, err.Error())
-	}
-}
-
 func TestFromIterator(t *testing.T) {
 	ours_expected, theirs := setup()
 	ours_got := New[string, int]()
