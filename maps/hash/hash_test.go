eddf4cd560709ac7cf105e188c9dccd3c29c00e3
Brian Reece <bdreece@mtu.edu>
2022-05-11 17:40:01 -0400

    Refactored hash map naming

---
+++ maps/hash/hash_test.go
@@ -0,0 +1,29 @@
+// MIT License
+// Copyright (c) 2022 Brian Reece
+
+package hash
+
+import "testing"
+
+const (
+	EXPECTED string = "expected %s (%d), got (%d)\n"
+	ERROR    string = "experienced error: \"%s\"\n"
+)
+
+func setup() (*Map[string, int], map[string]int) {
+	hashmap := New[string, int](0.75, 5)
+	pairs := make(map[string]int, 5)
+	pairs["apple"] = 1
+	pairs["banana"] = 2
+	pairs["cucumber"] = 3
+	pairs["date"] = 4
+	pairs["eclair"] = 5
+	return hashmap, pairs
+}
+
+func TestNew(t *testing.T) {
+	hashmap := New[string, int](0.75, 5)
+	if len(*hashmap.Vector) != 5 {
+		t.Errorf(EXPECTED, "len", 5, len(*hashmap.Vector))
+	}
+}
