8713d9e9f320c636adf40c5e505dc72af30c371f
Brian Reece <bdreece@mtu.edu>
2022-07-24 13:31:28 -0400

    Added some additional tests for maps/hash

--- maps/hash/hash_test.go
+++ maps/hash/hash_test.go
@@ -7,7 +7,7 @@ import "testing"
 
 const (
 	EXPECTED string = "expected %s (%d), got (%d)\n"
-	ERROR    string = "experienced error: \"%s\"\n"
+	ERROR    string = "experienced error: \"%v\"\n"
 )
 
 func setup() (*Map[string, int], map[string]int) {
@@ -25,5 +25,32 @@ func TestNew(t *testing.T) {
 	hashmap := New[string, int](0.75, 5)
 	if len(*hashmap.Vector) != 5 {
 		t.Errorf(EXPECTED, "len", 5, len(*hashmap.Vector))
+	}
+}
+
+func TestGet(t *testing.T) {
+	hashmap, pairs := setup()
+	got, err := hashmap.Get("cucumber")
+	expected := pairs["cucumber"]
+	if err != nil {
+		t.Errorf(ERROR, err)
+	}
+	if got == nil || *got != expected {
+		t.Errorf(EXPECTED, "value", expected, got)
+	}
+}
+
+func TestSet(t *testing.T) {
+	hashmap := New[string, int](0.75, 5)
+	err := hashmap.Set("banana", 2)
+	if err != nil {
+		t.Errorf(ERROR, err)
+	}
+	got, err := hashmap.Get("banana")
+	if err != nil {
+		t.Errorf(ERROR, err)
+	}
+	if *got != 2 {
+		t.Errorf(EXPECTED, "value", 2, *got)
 	}
 }
